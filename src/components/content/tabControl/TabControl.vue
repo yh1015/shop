a88bfbc2d5c1e90c74d30a521eae347b19ee1474
yh1015 <1752347692@qq.com>
2021-02-22 09:02:57 +0800

    Home

---
+++ src/components/content/tabControl/TabControl.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="tab-control">
+    <div v-for="(item,index) in titles" :key="index" class="tab-control-item">
+      <span>{{item}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TabControl',
+  props: {
+    titles: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .tab-control {
+    display: flex;
+    text-align: center;
+  }
+  .tab-control-item {
+    flex: 1;
+  }
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -5,16 +5,69 @@
       <div slot="center">购物街</div>
     </nav-bar> 
 
+    <home-swiper :banners="banners" />
+    <recommend-view :recommends="recommends" />
+    <feature-view/>
+    <tab-control :titles="titles" />
+
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      <li>5</li>
+      <li>6</li>
+      <li>7</li>
+      <li>8</li>
+      <li>9</li>
+      <li>10</li>
+      <li>11</li>
+      <li>12</li>
+      <li>13</li>
+      <li>14</li>
+      <li>15</li>
+      <li>16</li>
+      <li>17</li>
+      <li>18</li>
+      <li>19</li>
+      <li>20</li>
+    </ul>
+
   </div>
 </template>
 
 <script>
+import HomeSwiper from './childComp/HomeSwiper'
+import RecommendView from './childComp/RecommendView'
+import FeatureView from './childComp/FeatureView'
+
 import NavBar from 'components/common/navbar/NavBar'
+import TabControl from 'components/content/tabControl/TabControl'
+
 import {getHomeMultidata} from 'network/home'
 export default {
   name: 'Home',
+  data() {
+    return {
+      // 轮播图图片
+      banners: [],
+      recommends: [],
+      titles: ['流行','新款','精选']
+    }
+  },
+  created() {
+    getHomeMultidata().then(res => {
+      console.log(res)
+      this.banners = res.data.banner.list
+      this.recommends = res.data.recommend.list
+    })
+  },
   components: {
-    NavBar
+    HomeSwiper,
+    RecommendView,
+    FeatureView,
+    NavBar,
+    TabControl,
   }
 }
 </script>
